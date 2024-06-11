c4df9c3d365fd8a984079bc102a0923296bf16c8
Igor <ismyk256@gmail.com>
2024-06-11 11:35:22 +0300

    add introductory messages

--- src/main/java/ua/javarush/MyFirstTelegramBot.java
+++ src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -1,6 +1,7 @@
 package ua.javarush;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import static ua.javarush.TelegramBotUtils.createMessage;
 import static ua.javarush.TelegramBotUtils.getChatId;
+
 
 public class MyFirstTelegramBot extends TelegramLongPollingBot {
     Dotenv dotenv = Dotenv.load();
@@ -29,8 +31,23 @@ public class MyFirstTelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         long chatId = getChatId(update);
-        SendMessage message = createMessage(chatId, "Hello World!");
-        sendApiMethodAsync(message);
+
+        boolean isWelcomeWord = update.getMessage().getText().contains("привіт") || update.getMessage().getText().contains("Привіт");
+        boolean isMyName = update.getMessage().getText().contains("мене звуть") || update.getMessage().getText().contains("Мене звуть");
+
+        if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
+            SendMessage message = createMessage(chatId, "Welcome!");
+            sendApiMethodAsync(message);
+        } else if (update.hasMessage() && isWelcomeWord) {
+            SendMessage message = createMessage(chatId, "Привіт, як тебе звуть?");
+            sendApiMethodAsync(message);
+        } else if (update.hasMessage() && isMyName) {
+            SendMessage message = createMessage(chatId, "Радий знайомству, я — *Кіт*");
+            sendApiMethodAsync(message);
+        } else {
+            SendMessage message = createMessage(chatId, "_Meow_=^^=");
+            sendApiMethodAsync(message);
+        }
     }
 
     public static void main(String[] args) throws TelegramApiException {
